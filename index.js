79bc5191056bc3d0eded1c8a762e8f73c2877b1b
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-20 17:02:59 -0400

    end of day

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 // import mysql2 library
-const mysql = require('mysql2')
+const mysql = require("mysql2")
 
 // connect to our database
 const db = mysql.createConnection({
-    host: '34.170.135.140',
-    user: 'root',
-    password: 'BuildingSoFLo',
-    database: 'swecc-summer-2022',
+  host: "34.170.135.140",
+  user: "root",
+  password: "BuildingSoFLo",
+  database: "swecc-summer-2022",
 })
 
-// run a simple query 
-db.query('SELECT * FROM Books_jv', (err, results) => {
-    if(err){
-        console.log(err)
-    }
-    // output the results 
-    console.log(results)
-    db.end()
+//longer way
+/*
+function handleResults(err, results) {
+  if (err) {
+    console.log(err)
+  }
+  // output the results
+  console.log(results)
+  db.end()
+}
+*/
+
+// run a simple query
+db.query("SELECT * FROM Books_jv", (err, results) => {
+  if (err) {
+    console.log(err)
+  }
+  // output the results
+  console.log(results)
+  db.end()
 })
-
-
